f9793d352869a034538de494e2173762375f505d
Mehedi Hasan <mehedi.hasan23@brainstation-23.com>
2025-05-19 11:34:52 +0600

    refactor AppModule to remove unused AppService provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
@@ -10,7 +9,9 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [
+    AppController
+  ],
+  providers: [],
 })
 export class AppModule {}
